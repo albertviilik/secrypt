2327a309e73544d9e70b9b3a267157945ce1ed03
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2021-01-04 23:19:36 +0000

    Initial Commit

---
+++ secrypt/secryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  secryptApp.swift
+//  secrypt
+//
+//  Created by Albert Viilik on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct secryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
